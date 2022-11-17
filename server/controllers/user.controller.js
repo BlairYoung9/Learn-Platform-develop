e192552980a0c7bc7ba1f59b83a580ce89efa2a5
Blair Young <blairyoung009@gmail.com>
2022-11-17 11:18:56 -0800

    working on login

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -25,7 +25,7 @@ module.exports = {
             }
                 //compare passwords
             if(req.body.password != user.password)
-                return res.status(400);
+            return res.status(400).json({ msg: 'Password does not match' });
         }
         catch (err) {
             console.error("loginerr",err.message);
